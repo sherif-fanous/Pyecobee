1f7d36c94beaae03b52b7566bf1048969c698465
s_Fanous <sherif.fanous@gmail.com>
2017-05-23 12:54:14 -0400

    Fleshed out EcobeeApiException and EcobeeAuthorizationException to retain error details.

--- pyecobee/exceptions.py
+++ pyecobee/exceptions.py
@@ -3,11 +3,38 @@ class EcobeeException(Exception):
 
 
 class EcobeeApiException(EcobeeException):
-    pass
+    def __init__(self, message, status_code, status_message):
+        super(EcobeeApiException, self).__init__(message)
+        self._status_code = status_code
+        self._status_message = status_message
+
+    @property
+    def status_code(self):
+        return self._status_code
+
+    @property
+    def status_message(self):
+        return self._status_message
 
 
 class EcobeeAuthorizationException(EcobeeException):
-    pass
+    def __init__(self, message, error, error_description, error_uri):
+        super(EcobeeAuthorizationException, self).__init__(message)
+        self._error = error
+        self._error_description = error_description
+        self._error_uri = error_uri
+
+    @property
+    def error(self):
+        return self._error
+
+    @property
+    def error_description(self):
+        return self._error_description
+
+    @property
+    def error_uri(self):
+        return self._error_uri
 
 
 class EcobeeHttpException(EcobeeException):
